fca9d3be6427f71fc50d6d58e8fe5fe012b7e5ce
Kaif Ansari <kaifansari@example.com>
2026-05-18 12:07:02 +0530

    Fixed Google client ID

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,6 @@ link.rel = 'stylesheet';
 document.head.appendChild(link);
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
