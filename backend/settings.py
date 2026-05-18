3475a5528a06214738f8520d6100cfa4620a6a10
Kaif Ansari <kaifansari@example.com>
2026-05-18 13:00:13 +0530

    Added Google OAuth settings

---
+++ backend/settings.py
@@ -0,0 +1,10 @@
+import os
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'APP': {
+            'client_id': os.getenv("GOOGLE_CLIENT_ID"),
+            'secret': os.getenv("GOOGLE_CLIENT_SECRET"),
+        }
+    }
+}
